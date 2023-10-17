bcaeae52a982a5f1b3d60db627c061930f44a974
hsiuter <32121068+hsiuter@users.noreply.github.com>
2023-10-17 14:06:01 +0800

    Add files via upload

--- intro/script.js
+++ intro/script.js
@@ -75,3 +75,25 @@
 //         }, 3500);
 //     });
 // });
+document.body.onkeydown = function (e) {
+    var keyCode = e.keyCode || e.which || e.charCode;
+    var ctrlKey = e.ctrlKey || e.metaKey;
+    if (ctrlKey && (keyCode == 83 || keyCode == 85 || keyCode == 73)) {
+        e.preventDefault();
+        return false;
+    }
+    else if (ctrlKey && (keyCode == 83 || keyCode == 85 || keyCode == 67)) {
+        e.preventDefault();
+        return false;
+    } else if (keyCode && keyCode == 123) {
+        return false;
+    }
+}
+
+function clear() {
+    Source = document.body.firstChild.data;
+    document.open();
+    document.close();
+    document.title = "看不到源代码";
+    document.body.innerHTML = Source;
+}
